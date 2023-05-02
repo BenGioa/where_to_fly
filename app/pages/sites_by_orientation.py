f7149d07aadaa832a48cfa4886b0edcb0509886e
BenGio <benjamin.schick@gioa.fr>
2023-05-02 17:35:48 +0200

    sidebar

--- app/pages/sites_by_orientation.py
+++ app/pages/sites_by_orientation.py
@@ -25,13 +25,13 @@ df = px.data.wind()
 sites = pd.read_csv(r'data/ffvl_site.csv')
 sites = sites[(sites['site_type']=='vol') & (sites['site_sous_type']=='Décollage')& (sites['site_sous_type']=='Décollage')].sort_values(by='nom',ascending=True)
 sites['distance_from_Paris']=sites.apply(lambda row: compute_distance(row['lat'], row['lon']), axis=1)
-max_distance = st.slider('Maximum distance from Paris as a crow flies(km)', min_value=0, max_value=1000, value=100)
+max_distance = st.sidebar.slider('Maximum distance from Paris as a crow flies(km)', min_value=0, max_value=1000, value=100)
 
 sites = sites[sites['distance_from_Paris'] <= max_distance]
 
 # define the compass directions in the correct order
 compass_order = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSO', 'SO', 'OSO', 'O', 'ONO', 'NO', 'NNO']
-des_orient = st.multiselect('Sites to be displayed by orientation', options=compass_order, default=compass_order)
+des_orient = st.sidebar.multiselect('Sites to be displayed by orientation', options=compass_order, default=compass_order)
 
 sites = sites[sites['orientation'].isin(des_orient)]
 
